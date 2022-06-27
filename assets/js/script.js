81b0619a26e198999f1dea77a22262c524128277
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-06-27 17:28:04 -0400

    add start button for loop

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+var todoInput = document.querySelector("#todo-text");
+var todoForm = document.querySelector("#todo-form");
+var todoList = document.querySelector("#todo-list");
+var todoCountSpan = document.querySelector("#todo-count");
+
+var todos = [];
+
+for (var i = 0; i < todos.length; i++) {
+    var todo = todos[i];
+
+    var li = document.createElement("li");
+    li.textContent = todo;
+    li.setAttribute("data-index", i);
+
+    var button = document.createElement("button");
+    button.textContent = "Complete ✔️";
+
+    li.appendChild(button);
+    todoList.appendChild(li);
+  }
